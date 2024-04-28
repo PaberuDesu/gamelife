080220cbc89874bb34dba495205d448cb1565dc3
4218 Николаев П.С <pashan1ckolaeff@yandex.ru>
2024-04-29 02:22:18 +0300

    Leaving settings and save/load
    Now you can go from the settings or from the save/load menu to the "escape" menu by using the "esc" button. There will be game screen without settings or save/load menu on the background.

--- game life code/Assets/Scripts/escape.cs
+++ game life code/Assets/Scripts/escape.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 public class escape : MonoBehaviour
 {
     [SerializeField] private GameObject escape_menu;
+    [SerializeField] private GameObject settings_menu;
+    [SerializeField] private GameObject saveload_menu;
+
     void Update() {
         if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (settings_menu.activeSelf) settings_menu.SetActive(false);
+            else if (saveload_menu.activeSelf) saveload_menu.SetActive(false);
             escape_menu.SetActive(!escape_menu.activeSelf);
         }
     }
